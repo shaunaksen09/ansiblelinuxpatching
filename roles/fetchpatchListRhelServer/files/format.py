46889064ad7ad54f2de4b902627666cfbb19dbc7
Shaunak Sen <shaunak.sen@lucidworks.com>
2021-06-02 23:06:29 +0530

    Patching repo

---
+++ roles/fetchpatchListRhelServer/files/format.py
@@ -0,0 +1,8 @@
+import sys
+args = sys.argv[1:]
+with open( sys.argv[1],'r') as f:
+	contents = f.readline()
+list1 = contents.split(',')
+with open('/home/ansible/ansible-patching/OSPatching/RedHat/patchListRhel.csv','w+') as f2:
+	for item in list1:
+		f2.write("%s\n" %item)
